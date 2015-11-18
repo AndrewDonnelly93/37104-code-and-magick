5c43eabc0b8afb71ad00b14c71a180b1e29c5b8d
AndrewDubovtsev <andrey.dubovtsev@gmail.com>
2015-11-18 15:51:15 +0300

    Changed check

--- js/check.js
+++ js/check.js
@@ -1,43 +1,41 @@
 function getArraySum(a){
   var sum = 0;
-  a.forEach(function(item, i, a) {
+  a.forEach(function(item) {
     sum += item;
   });
   return sum;
 }
 
-function getArraysProduct(a,b) {
+function getArraysProduct(a, b) {
   var product = 0;
-  a.forEach(function(item, i, a) {
+  a.forEach(function(item, i) {
     product += item * b[i];
   });
   return product;
 }
 
-function getMessage(a,b) {
-  var typeA = typeof a;
-  var typeB = typeof b;
-  switch (typeA) {
-    case 'boolean':
+function getMessage(a, b) {
+  switch (true) {
+    case (Array.isArray(a) && Array.isArray(b)):
+      var product = getArraysProduct(a, b);
+      return 'Я прошёл ' + product + ' метров';
+      break;
+    case (Array.isArray(a)):
+      var sum = getArraySum(a);
+      return 'Я прошёл ' + sum + ' шагов';
+      break;
+    case (typeof a === 'number'):
+      return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+      break;
+    case (typeof a === 'boolean'):
       if (a) {
         return 'Я попал в ' + b;
       } else {
         return 'Я никуда не попал';
       }
       break;
-    case 'number':
-      return 'Я прыгнул на ' + a*100 + ' сантиметров';
-      break;
-    case 'object':
-      if (typeA !== typeB) {
-        var sum = getArraySum(a);
-        return 'Я прошёл ' + sum + ' шагов';
-      } else {
-        var product = getArraysProduct(a,b);
-        return 'Я прошёл ' + product + ' метров';
-      }
-      break;
     default:
+      return 'Я не знаю, что вы хотели сделать.';
       break;
   }
 }
